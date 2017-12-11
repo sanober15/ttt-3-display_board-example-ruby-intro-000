09ea49f3618c7d1cb66baeb3ea4e44afb5983a14
sanober riaz <sanober@sanober.me>
2017-12-11 01:49:09 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  cell = "   "
-  row = "#{cell}|#{cell}|#{cell}"
-  puts row
-  11.times puts "-"
-  puts row
-  11.times puts "-"
-  puts row
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
 
 end
